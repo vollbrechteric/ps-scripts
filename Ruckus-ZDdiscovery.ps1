7a57373f833f27357cc3369f76beb104a1708abc
Eric Vollbrecht <vollbrechteric@hotmail.com>
2015-01-07 00:17:23 -0600

    updated zddomain split

--- Ruckus-ZDdiscovery.ps1
+++ Ruckus-ZDdiscovery.ps1
@@ -4,7 +4,7 @@ Run this script on a server with the DHCP and DNS role installed.
 #>
 $zdip = Read-Host -Prompt 'IP address of ZoneDirector?'
 $zdfqdn = Read-Host -Prompt 'FQDN of the ZoneDirector?'
-$zddomin = 
+$zddomin = $zdfqdn -split '.', 0, 'simplematch'
 
 #region DHCP Option 43 configuration
 # TODO: Check if DHCP server role is installed. Fail if false.
