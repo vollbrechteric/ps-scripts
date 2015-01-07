2d65647b83d034351e5af2f9fcc072a98c389f5e
Eric Vollbrecht <vollbrechteric@hotmail.com>
2015-01-06 23:58:30 -0600

    Update Author

--- Ruckus-ZDdiscovery.ps1
+++ Ruckus-ZDdiscovery.ps1
@@ -1,11 +1,26 @@
-﻿<#
-Setup a network for a ZoneDirector
-DHCP discovery with option 43
-  setup vendor class of 'Ruckus CPE'
-  setup option 43
-  Set predefined Options
-  Assign to scope
+﻿<# Configures network so that Ruckus ZoneFlex APs can more easily find Ruckus ZoneDirectors.
 
-DNS lookup for zonedirector.<domain>
-
+Run this script on a server with the DHCP and DNS role installed.
 #>
+$zdip = Read-Host -Prompt 'IP address of ZoneDirector?'
+$zdfqdn = Read-Host -Prompt 'FQDN of the ZoneDirector?'
+$zddomin = 
+
+#region DHCP Option 43 configuration
+# TODO: Check if DHCP server role is installed. Fail if false.
+# DHCP discovery with option 43
+# setup vendor class of 'Ruckus CPE'
+Add-DhcpServerv4Class -Data 'Ruckus CPE' -Name 'Ruckus CPE' -Type Vendor -Description 'Ruckus ZoneDirector discovery for APs'
+
+# setup option 43
+# Set predefined Options
+# Assign to scope
+
+#endregion DHCP Option 43 configuration
+
+#region DNS hostname configuration
+# TODO: Check if DNS role is already installed. If false fail.
+# TODO: Check if record already exists. If true fail.
+Resolve-DnsName -Name zonedirector
+
+#endregion DNS hostname configuration
