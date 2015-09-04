10a54ab7f0eea38cf5ad5b582af9257bd4125ab3
Eric Vollbrecht <vollbrechteric@hotmail.com>
2015-09-03 23:04:03 -0500

    new script

---
+++ netbindings.ps1
@@ -0,0 +1,9 @@
+﻿# disable or enable netowrk bindings
+# Get the net adapters
+Get-NetAdapter |
+Out-GridView -Title 'Choose the adapter to make changes' -PassThru |
+# Get the bindings for selected adapters
+ForEach-Object { Get-NetAdapterBinding -Name $_.name } |
+Out-GridView -PassThru -Title 'Choose bindings to disable or enable' |
+# disable or enable network bindings
+ForEach-Object { Disable-NetAdapterBinding -Name "$_.name" -ComponentID $_.ComponentID }
