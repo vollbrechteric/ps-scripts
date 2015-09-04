203efb47e3b577a605d1d270ce18fcf8b7fe84c0
Eric Vollbrecht <vollbrechteric@hotmail.com>
2015-09-03 23:13:59 -0500

    Update header info

--- netbindings.ps1
+++ netbindings.ps1
@@ -1,7 +1,19 @@
-﻿# disable or enable netowrk bindings
+﻿#requires -version 4.0
+<#
+Created by Eric Vollbrecht vollbrechteric@hotmail.com
+
+https://github.com/vollbrechteric/ps-scripts
+
+Get network adapters and then netbindings based on selection.
+Flip the setting from true to false or false to true
+
+TODO: setup the if..then
+
+TODO: Error handling
+#>
 # Get the net adapters
 Get-NetAdapter |
-Out-GridView -Title 'Choose the adapter to make changes' -PassThru |
+Out-GridView -Title 'Choose the adapter(s) to change' -PassThru |
 # Get the bindings for selected adapters
 ForEach-Object { Get-NetAdapterBinding -Name $_.name } |
 Out-GridView -PassThru -Title 'Choose bindings to disable or enable' |
