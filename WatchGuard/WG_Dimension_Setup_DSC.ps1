71459fb94baa3a4275c24153f8283a236cd4aeb3
Eric Vollbrecht <vollbrechteric@hotmail.com>
2016-12-18 21:51:56 -0600

    New DSC
    Start converting existing script to DSC for testing.

---
+++ WatchGuard/WG_Dimension_Setup_DSC.ps1
@@ -0,0 +1,38 @@
+﻿#requires -version 4.0
+<#
+        Created by Eric Vollbrecht vollbrechteric@hotmail.com
+
+        https://github.com/vollbrechteric/ps-scripts
+
+        Desired State Configurtion script to configure WatchGuard Dimension server on Microsoft Hyper-V 2016.
+
+#>
+
+configuration WGDimension
+{
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node $AllNodes.Where{$_.Role -eq 'WebServer'}.NodeName
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        WindowsFeature HyperV
+        {
+            Ensure = 'Present'
+            Name = 'Hyper-V'
+        }
+
+        File FriendlyName
+        {
+            Ensure = 'Present'
+            SourcePath = $SourcePath
+            DestinationPath = $DestinationPath
+            Type = 'Directory'
+            DependsOn = '[WindowsFeature]FriendlyName'
+        }
+        
+        
+    }
+}
+
+# ConfigurationName -configurationData <path to ConfigurationData (.psd1) file>
