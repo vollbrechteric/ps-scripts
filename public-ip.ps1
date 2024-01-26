68611509e8bf82540465393e88c24a3a86de636e
Eric Vollbrecht <vollbrechteric@hotmail.com>
2024-01-26 14:27:46 -0600

    add random sites

--- public-ip.ps1
+++ public-ip.ps1
@@ -1,1 +1,16 @@
-﻿(Invoke-RestMethod -Uri 'https://ipinfo.io' -UseBasicParsing).ip
+﻿$uri = Get-Random -InputObject @(
+    'https://ipinfo.io/ip'
+    'https://icanhazip.com'
+    'https://ident.me'
+    'https://trackip.net/ip'
+    'https://checkip.amazonaws.com'
+    'https://myexternalip.com/raw'
+    'https://wtfismyip.com/text'
+    'https://api.seeip.org'
+    'https://api.ipify.org'
+    'https://secure.informaction.com/ipecho'
+    'https://ifconfig.me/ip'
+)
+
+$IP = Invoke-RestMethod -Uri "$uri"
+$IP
